06b581eff6581fdd56972f464adfe9b830ff893b
Alexey <malyshev@webshop.ru>
2024-07-15 14:53:02 +0300

    preloader

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -25,7 +25,7 @@ const Preloader = () => {
       store.switchLoading();
       loading.value = false;
       bodyScrollBar.updatePluginOptions("lock", { lock: false });
-   }, 1400);
+   }, 1800);
 };
 
 const mobileAnimation = () => {
