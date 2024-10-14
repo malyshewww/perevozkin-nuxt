cc1565a6adaa5fbccdcce8b107f9189aec965730
Alexey <malyshev@webshop.ru>
2024-10-14 12:01:24 +0300

    server changes

--- pages/index.vue
+++ pages/index.vue
@@ -11,75 +11,76 @@
 import initCustomScrollbar from "~/utils/customScrollbar";
 
 defineProps({
-   main: {
-      required: true,
-   },
+  main: {
+    required: true,
+  },
 });
 
 definePageMeta({
-   layout: "custom",
+  layout: "custom",
 });
 
 const runtimeConfig = useRuntimeConfig();
 const {
-   data: front,
-   status,
-   error,
+  data: front,
+  status,
+  error,
 } = await useAsyncData(
-   "front",
-   () =>
-      $fetch(
-         `${runtimeConfig.public.apiBase}/wsapi/packs/front?_format=json`,
-         {}
-      ),
-   {
-      transform: ({ data, metatag }) => {
-         const metadata = useGenerateMeta(metatag.html_head);
-         const { acc: meta, title } = metadata;
-         return {
-            main: {
-               advantages: data.advantages[0].field_advantages,
-               catalogFront: data.catalog_front,
-               serviceFront: data.services_front,
-               frontInfo: {
-                  advantageText: data.front_info[0].field_advantage_text,
-                  partners: data.front_info[0].field_partners,
-                  stocks: data.front_info[0].field_stocks,
-               },
-            },
-            meta,
-            title,
-         };
-      },
-   }
+  "front",
+  () =>
+    $fetch(
+      `${runtimeConfig.public.apiBase}/wsapi/packs/front?_format=json`,
+      {}
+    ),
+  {
+    transform: ({ data, metatag }) => {
+      const metadata = useGenerateMeta(metatag.html_head);
+      const { acc: meta, title } = metadata;
+      console.log(data);
+      return {
+        main: {
+          advantages: data.advantages[0].field_advantages,
+          catalogFront: data.catalog_front,
+          serviceFront: data.services_front,
+          frontInfo: {
+            advantageText: data.front_info[0].field_advantage_text,
+            partners: data.front_info[0].field_partners,
+            stocks: data.front_info[0].field_stocks,
+          },
+        },
+        meta,
+        title,
+      };
+    },
+  }
 );
 
 useHead({
-   title: front.value.title,
-   meta: [...front.value.meta],
-   bodyAttrs: {
-      class: `page--home`,
-   },
+  title: front.value.title,
+  meta: [...front.value.meta],
+  bodyAttrs: {
+    class: `page--home`,
+  },
 });
 
 onMounted(() => {
-   const { bodyScrollBar } = initCustomScrollbar();
-   if (!window.location.hash) {
-      bodyScrollBar.scrollTo(0, 0, 100);
-   } else {
-      const sectionSale = document.querySelector(".main-sale");
-      if (sectionSale) {
-         let itemY = sectionSale.offsetTop + 100;
-         console.log(itemY);
-         bodyScrollBar.scrollTo(0, itemY, 600);
-         // const scrollToHere = sectionSale.offsetTop + bodyScrollBar.offset.y;
-         // bodyScrollBar.scrollTo(
-         //    0,
-         //       scrollToHere,
-         //    100
-         // );
-      }
-   }
+  const { bodyScrollBar } = initCustomScrollbar();
+  if (!window.location.hash) {
+    bodyScrollBar.scrollTo(0, 0, 100);
+  } else {
+    const sectionSale = document.querySelector(".main-sale");
+    if (sectionSale) {
+      let itemY = sectionSale.offsetTop + 100;
+      console.log(itemY);
+      bodyScrollBar.scrollTo(0, itemY, 600);
+      // const scrollToHere = sectionSale.offsetTop + bodyScrollBar.offset.y;
+      // bodyScrollBar.scrollTo(
+      //    0,
+      //       scrollToHere,
+      //    100
+      // );
+    }
+  }
 });
 </script>
 
